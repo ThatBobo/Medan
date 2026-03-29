d685e0a5dbfc762b91b3c484243803d4bf5c2cd6
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-29 18:26:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound"
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Triper/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
