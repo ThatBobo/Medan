918e513ce90635140805eb13f7e1c9bef9af2d35
IamBobo <abdulsamad.arilewola@gmail.com>
2026-04-01 15:33:41 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
+import Install from "./pages/Install";
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/install" element={<Install />} />
       </Routes>
     </BrowserRouter>
   );
